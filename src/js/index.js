500c1fa8f01cdd25f3de094a8980f9862b724a25
mazdim131 <syandanadimas7.@gmail.com>
2026-04-17 15:27:04 +0700

    third commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,11 @@
-/* 
-NOTES:
-to make this asynchronous test work, you need to change parameters in the HTML file. 
-take a look at click listener on the button, you will see "babypack".
-change it to "starterpack". 
-done!
-*/
+const express = require('express')
+const app = express()
+const port = 3000
 
-console.log('javascript ready...')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-//! Responsive Navigation BAR 🎉
-const showMobileNav = () => {
-  var navbar = document.getElementById("navbar");
-  navbar.className === "navibar" ? navbar.className += " responsive" : navbar.className = "navibar"
-}
-
-//! Notification Bell 
-const notification = (pack) => {
-    alert('notification checking..., please click OK to continue')
-    return new Promise((success, failed) => {
-        if (pack != "starterpack") failed("your pack is not starterpack, please read the notes on js file")
-        setTimeout(() => {
-            success("Notification Success! 👏")
-        }, 1200)
-    })
-}
-
-//! asynchronous trigger for notification bell
-async function showPopup(params) {
-    try {
-        const popup = await notification(params);
-        alert(popup)
-    } catch (err) {
-        alert(err)
-    }
-}
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
